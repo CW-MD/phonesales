2e8da5000344a06fc0b97a6737f4d85a96a5f70f
Chad Dayton <Cdayton96@gmail.com>
2021-09-17 14:17:42 -0400

    feat: accepts user input, returns total price

--- phoneSales.cpp
+++ phoneSales.cpp
@@ -7,19 +7,22 @@
 using namespace std;
 
 int main(){
-int choice;
 
-int chip;
-int campixels;
-int screenres;
-string dolbytype;
-int  battlife;
-int phonewt;
-bool ports;
-string os;
-bool waterproof;
-bool wirecharge;
-const float BASEPRICE = 800.25;
+    //data vars
+int chip = 32;
+int campixels = 1080;
+int screenres = 1080;
+string dolbytype = "Dolby Digital";
+int  battlife = 60;
+int phonewt = 125;
+bool ports = false;
+string os = "Android";
+bool waterproof = false;
+bool wirecharge = false;
+const float BASEPRICE = 826.59;
+
+    //working vars
+int choice = 0;
 float totalprice = BASEPRICE;
 
 //Base Model
@@ -32,7 +35,62 @@ cout << "Please choose the phone options. \n\n";
 
 cout << "Chip (1 - 64hz || 2 - 128hz)";
 cin >> choice;
-if(choice == 2) totalprice +=25;
+if(choice == 2){
+    chip = 128;
+    totalprice +=25;
+}
+ cout << "Camera (1 - 1080p || 2 - 4k)";
+cin >> choice;
+if(choice == 2){
+    campixels = 4000;
+    totalprice +=200;
+} cout << "Screen (1 - 1080p || 2 - 4k)";
+cin >> choice;
+if(choice == 2){
+    screenres = 4000;
+    totalprice +=200;
+} cout << "Sound (1 - Dolby Digital || 2 - Dolby HD)";
+cin >> choice;
+if(choice == 2){
+    dolbytype = "Dolby HD";
+    totalprice +=50;
+} cout << "Chip (1 - 64hz || 2 - 128hz)";
+cin >> choice;
+if(choice == 2){
+    chip = 128;
+    totalprice +=25;
+} cout << "Battery Life (1 - 60hr || 2 - 120hr)";
+cin >> choice;
+if(choice == 2){
+    battlife = 120;
+    totalprice +=100;
+} cout << "phone weight (1 - 125g || 2 - 80g)";
+cin >> choice;
+if(choice == 2){
+    phonewt = 80;
+    totalprice +=50;
+} cout << "Additional Ports? (1 - No || 2 - Yes)";
+cin >> choice;
+if(choice == 2){
+    ports = true;
+    totalprice +=50;
+} cout << "OS (1 - Android || 2 - IOS)";
+cin >> choice;
+if(choice == 2){
+    os = "IOS";
+    totalprice +=50;
+} cout << "Waterproof? (1 - No || 2 - Yes)";
+cin >> choice;
+if(choice == 2){
+    waterproof = true;
+    totalprice +=100;
+} cout << "Wireless Charging? (1 - No || 2 - Yes)";
+cin >> choice;
+if(choice == 2){
+    wirecharge = true;
+    totalprice +=100;
+} 
+cout <<"Your phone will cost $" << totalprice << "\n";
 
 return 0;
 }
